237c550a80576e04a9cef13d16b2e29781050703
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-07-05 16:40:13 -0500

    crud

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyAR579z9A0sIAo_PT4F2uwLgA95Bm9Vk14",
+    authDomain: "crub-apps.firebaseapp.com",
+    projectId: "crub-apps",
+    storageBucket: "crub-apps.appspot.com",
+    messagingSenderId: "524293858875",
+    appId: "1:524293858875:web:85dfe567820b032df59e1e",
+    measurementId: "G-PCE1V997MY"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyAR579z9A0sIAo_PT4F2uwLgA95Bm9Vk14",
+    authDomain: "crub-apps.firebaseapp.com",
+    projectId: "crub-apps",
+    storageBucket: "crub-apps.appspot.com",
+    messagingSenderId: "524293858875",
+    appId: "1:524293858875:web:85dfe567820b032df59e1e",
+    measurementId: "G-PCE1V997MY"
+  }
 };
 
 /*
