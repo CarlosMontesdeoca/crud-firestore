7881865c648b5b50bffef44c8c5a43a638d671dd
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-07-05 20:48:11 -0500

    deber crud firebase

--- src/app/create-todo/create-todo.page.ts
+++ src/app/create-todo/create-todo.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CrudService } from '../services/crud.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-create-todo',
   templateUrl: './create-todo.page.html',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTodoPage implements OnInit {
 
-  constructor() { }
+  todoForm: FormGroup;
+
+  constructor(
+    private crudService: CrudService,
+    public formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      title: [''],
+      description: ['']
+    })
   }
 
+  onSubmit() {
+    if ( !this.todoForm.valid ) {
+      return false;
+    } else {
+      this.crudService.create( this.todoForm.value )
+      .then(() => {
+        this.todoForm.reset();
+        this.router.navigate([ '/todo-list' ]);
+      }) .catch (( error ) => {
+        console.log( error )
+      });
+    }
+  }
 }
